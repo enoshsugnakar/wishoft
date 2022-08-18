58e1655f90835d8ae61b1b06f0ed3cce0b4bd50d
Enosh Sugnakar <111285971+enoshsugnakar@users.noreply.github.com>
2022-08-19 01:49:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+  
+}
