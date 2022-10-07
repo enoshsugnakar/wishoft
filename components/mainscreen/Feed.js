23421963e8732b8ed77bb78b5caf876a05ad28f8
Enosh Sugnakar <111285971+enoshsugnakar@users.noreply.github.com>
2022-10-07 20:38:26 +0530

    Update Feed.js

--- components/mainscreen/Feed.js
+++ components/mainscreen/Feed.js
@@ -3,7 +3,7 @@
 export default function Feed() {
     return (
         <div className=" rounded-lg p-20 mt-4 dark:bg-slate-900">
-            HONEsCREEN
+            HOME SCREEN
         </div>
     );
 }
